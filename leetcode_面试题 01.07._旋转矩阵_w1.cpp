4fea1948012bc9180862351cf69d710db72338a6
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-29 15:24:29 +0800

    Add files via upload

---
+++ leetcode_面试题 01.07._旋转矩阵_w1.cpp
@@ -0,0 +1,105 @@
+﻿/* ------------------------------------------------------------------|
+给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+
+不占用额外内存空间能否做到？
+
+ 
+
+示例 1:
+
+给定 matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+示例 2:
+
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+注意：本题与主站 48 题相同：https://leetcode-cn.com/problems/rotate-image/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rotate-matrix-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.9 MB, 在所有 C++ 提交中击败了45.83%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n / 2; ++i) {
+        for (int j = i; j < n - 1 - i; ++j) {
+            swap(matrix[i][j], matrix[n - 1 - i][n - 1 - j]);
+
+            swap(matrix[i][j], matrix[n - 1 - j][i]);
+            swap(matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j]);
+            
+        }
+    }
+    return;
+}
